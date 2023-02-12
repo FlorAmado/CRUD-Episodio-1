ee4e96f5484fb67508544da87a8b37b8e9df99e1
FlorenciaAmado <amadoflorencia8@gmail.com>
2023-02-12 19:01:58 -0300

    Agrega funcion de CREAR producto

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,3 +1,4 @@
+const { log } = require('console');
 const fs = require('fs');
 const path = require('path');
 
@@ -5,21 +6,33 @@ const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const precioFinal = products.map(product => product.price - (product.price * (product.discount/100)));
 
 const controller = {
 	index: (req, res) => {
 		// Do the magic
 		const inSale =products.filter(product=> product.category ==='in-sale')
-		const visited =products.filter(product=> product.category ==='vidited')
+		const visited =products.filter(product=> product.category ==='visited')
+		
 		return res.render('index',{
 			inSale,
-			visited,
+			visited,	
+			toThousand,
+			precioFinal,
 		
-		})
+		
+		})				
 	},
 	search: (req, res) => {
 		// Do the magic
-		return res.render('search')
+		//console.log(req.query);
+		const {keywords} = req.query;
+		const productsFiltered = products.filter(product => product.name.toLowerCase().includes(keywords.toLowerCase()) || product.description.toLowerCase().includes(keywords.toLowerCase()))
+		return res.render('results',{
+			productsFiltered,
+			toThousand,
+			keywords	
+		})
 	},
 };
 
--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { stringify } = require('querystring');
 
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
@@ -9,55 +10,74 @@ const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 const controller = {
 	// Root - Show all products
 	index: (req, res) => {
-		// Do the magic
-		return res.render('index',{
-			
+		const precioFinal = products.map(product => product.price - (product.price * (product.discount/100)));
+		return res.render('products',{
+			products,
+			toThousand,
+			precioFinal,
 		})
 	},
 
 	// Detail - Detail from one product
 	detail: (req, res) => {
-		// Do the magic
 	const { id } = req.params;
     const product = products.find(product => product.id === +id);
 	const precioFinal = product.price - (product.price * (product.discount/100));
-	/* const total = if (product.discount >0) {
-		return product
-	} */
-	
 	return res.render('detail',{
-			title:"Detalle del producto",
-			...product,
-			precioFinal,
-		})
+		...product,
+		precioFinal,
+		toThousand,
+
+	})
+	
 	},
 
 	// Create - Form to create
 	create: (req, res) => {
-		// Do the magic
-		return res.render('create')
+	
+	
+		return res.render('product-create-form',{
+			title:"Creacion del producto",
+			
+		})
 	},
 	
 	// Create -  Method to store
 	store: (req, res) => {
-		// Do the magic
-		return res.render('store')
+	
+	const {id,name,price,discount,description,category}= req.body;
+
+	const newProduct = {
+		id: products[products.length -1 ].id +1,
+		name: name.trim(),
+		price: +price.trim(),
+		discount: +discount.trim(),
+		image: null,
+		description: description.trim(),
+		category: category,
+	}
+
+	products.push(newProduct);
+
+	fs.writeFileSync('./src/data/productsDataBase',JSON.stringify((products, null, 3),'utf-8'))
+
+		return res.redirect('/products');
 	},
 
 	// Update - Form to edit
 	edit: (req, res) => {
-		// Do the magic
+		
 		return res.render('edit')
 	},
 	// Update - Method to update
 	update: (req, res) => {
-		// Do the magic
+		
 		return res.render('update')
 	},
 
 	// Delete - Delete one product from DB
 	destroy : (req, res) => {
-		// Do the magic
+		
 		return res.render('delete')
 	}
 };
